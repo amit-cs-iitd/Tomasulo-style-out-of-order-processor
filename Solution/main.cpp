9dec11a5483ecf8059ba3a26fac43c387f7f9325
Amit Singh Nitharwal <cs1240308@cse.iitd.ac.in>
2026-04-19 20:29:55 +0530

    changed main to show output of all the cycles. deleted the checker and all testcases since they aren't compatible with the new output format. Will add new testcases later.

--- Solution/main.cpp
+++ Solution/main.cpp
@@ -1,52 +1,56 @@
 #include <iostream>
 #include <string>
+
 #include "Processor.h"
 
 using namespace std;
+
+static void dumpCycleMemory(const Processor& cpu, int cycle) {
+  cout << "cycle " << cycle << ": ";
+  for (int i = 0; i < static_cast<int>(cpu.Memory.size()); i++) {
+    cout << cpu.Memory[i] << " ";
+  }
+  cout << "\n";
+}
+
 int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    cerr << "Usage: ./main <filename.s>\n";
+    return 1;
+  }
 
-    if (argc < 2) {
-        cerr << "Usage: ./main <filename.s> [-cycles N]\n";
-        return 1;
-    }
+  ProcessorConfig config;
+  Processor cpu(config);
 
-    int max_cycles = -1;
-    if (argc == 4 && string(argv[2]) == "-cycles") {
-        max_cycles = stoi(argv[3]);
-    }
+  try {
+    cpu.loadProgram(argv[1]);
+  } catch (...) {
+    cerr << "Failed to parse instruction file.\n";
+    return 1;
+  }
 
-    ProcessorConfig config;
-    Processor cpu = Processor(config);
-    
-    try {
-        cpu.loadProgram(argv[1]);
-    } catch (...) {
-        cerr << "Failed to parse instruction file.\n";
-        return 1;
+  dumpCycleMemory(cpu, 0);
+  while (true) {
+    bool keep_running = cpu.step();
+    dumpCycleMemory(cpu, cpu.clock_cycle);
+    if (!keep_running) {
+      break;
     }
+  }
 
-    int cycle_count = 0;
-    while (cpu.step()) {
-        cycle_count++;
-        if (max_cycles != -1 && cycle_count == max_cycles) {
-            cout << "\n[!] Execution halted at cycle limit: " << max_cycles << "\n";
-            break;
-        }
-    }
+  if (cpu.exception) {
+    cout << "\n[+] Execution halted due to exception after " << cpu.clock_cycle
+         << " cycles.\n";
+  } else {
+    cout << "\n[+] Execution complete naturally in " << cpu.clock_cycle
+         << " cycles.\n";
+  }
 
-    if (max_cycles == -1) {
-        if (cpu.exception) {
-            cout << "\n[+] Execution halted due to exception after " << cpu.clock_cycle << " cycles.\n";
-        }
-        else {
-            cout << "\n[+] Execution complete naturally in " << cpu.clock_cycle << " cycles.\n";
-        }
-    }
+  cpu.dumpArchitecturalState();
+  for (int i = 0; i < static_cast<int>(cpu.Memory.size()); i++) {
+    cout << cpu.Memory[i] << " ";
+  }
+  cout << "\n";
 
-    cpu.dumpArchitecturalState();
-    for (int i=0;i<cpu.Memory.size();i++) {
-        cout << cpu.Memory[i] << " ";
-    }
-    cout << endl;
-    return 0;
+  return 0;
 }
